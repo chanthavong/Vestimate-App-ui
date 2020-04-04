c92f4d81b51fcf3127b270406b45c44510e9bc90
Mark <chanthavong_12@hotmail.com>
2020-04-04 16:10:24 +0700

    update

---
+++ android/app/src/main/kotlin/ui/app/hello/ai/mark/flutter_app_restimate_ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package ui.app.hello.ai.mark.flutter_app_restimate_ui
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  List<Mycard> mycard = [
+    Mycard(Icons.shopping_cart, 'Buying', true),
+    Mycard(Icons.shop, 'Selling', false),
+    Mycard(Icons.account_balance, 'Trades', false),
+    Mycard(Icons.play_circle_outline, 'Videos', false),
+    Mycard(Icons.people_outline, 'Deal', false),
+    Mycard(Icons.bookmark_border, 'Case Study', false),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xfff6f7f9),
+      appBar: AppBar(
+        title: Text('Vestimate'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Choose your area',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.black54,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 8,
+                children: mycard
+                    .map(
+                      (e) => Card(
+                        color: e.isActive ? Colors.deepPurple : null,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(
+                              e.icon,
+                              size: 50,
+                              color:
+                                  e.isActive ? Colors.white : Colors.deepPurple,
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              e.title,
+                              style: TextStyle(
+                                  color:
+                                      e.isActive ? Colors.white : Colors.grey),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Mycard {
+  final icon;
+  final title;
+  bool isActive = false;
+
+  Mycard(this.icon, this.title, this.isActive);
+}
