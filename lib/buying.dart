612f9ff7c8fb22e913c98033261e7038941131a2
Mark <chanthavong_12@hotmail.com>
2020-04-07 17:49:46 +0700

    add listitle, avatar, shadow, borderRadius, stack layout and posistioned

--- lib/buying.dart
+++ lib/buying.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
 
+class Product {
+  final image;
+  final name;
+  final price;
+
+  Product(this.image, this.name, this.price);
+}
+
 class Buying extends StatelessWidget {
+  List products = [
+    Product(
+        'https://lh3.googleusercontent.com/proxy/2gvngPLBf-XuU77M62l_Wcr-8YSArI-PVfKHVKhf2zqwv2c8WAnt-EWwK0nUTtQM4SOT4ItbAd4rmRDtyg5k5htcIT_4Mu6X2FmbWTSZT6Uj5BJuSWMwy5NplZO15iA9BjrstZYI',
+        'Camera canon',
+        2000),
+    Product(
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSdygFdB_FfadQmmrDZUgLeJTILZBTU0d9Ffs3mLpYSh2rulaJo&usqp=CAU',
+        'Camera canon',
+        2000),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xfff6f7f9),
       appBar: AppBar(
         title: Text('Buyings'),
       ),
       body: ListView(
         children: <Widget>[
           Container(
+            margin: EdgeInsets.symmetric(vertical: 2, horizontal: 10),
             height: 50,
             child: ListView(
               scrollDirection: Axis.horizontal,
@@ -33,7 +53,66 @@ class Buying extends StatelessWidget {
                       ))
                   .toList(),
             ),
-          )
+          ),
+          Column(
+              children: products
+                  .map(
+                    (e) => Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(13),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey[200],
+                              blurRadius: 10,
+                              spreadRadius: 3,
+                              offset: Offset(3, 4))
+                        ],
+                      ),
+                      child: ListTile(
+                        leading: Image.network(
+                          e.image,
+                          fit: BoxFit.cover,
+                          width: 90,
+                          height: 100,
+                        ),
+                        title: Text(
+                          e.name,
+                          style: TextStyle(fontSize: 25),
+                        ),
+                        subtitle: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text("\$ " + e.price.toString()),
+                            SizedBox(height: 10),
+                            Container(
+                              height: 40,
+                              child: Stack(
+                                children: <Widget>[
+                                  Positioned(
+                                      left: 20,
+                                      bottom: 0,
+                                      child: CircleAvatar(
+                                          backgroundColor: Colors.green,
+                                          child: Image.network(
+                                              'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRxHTyqjCdnZsEM-EkMvn3FDBkDADcaEZ3GN1YEdWFToAJm83nX&usqp=CAU'))),
+                                  Positioned(
+                                    left: 0,
+                                    bottom: 0,
+                                    child: CircleAvatar(child: Text('a')),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                  .toList())
         ],
       ),
     );
